e86a36eb6dff67a91f3d6b9488a394bbd2a37cee
Valdir Silva <valdipiresba@gmail.com>
2023-06-29 00:00:22 -0300

    Adiciona rotas listar e criar transacao

--- mvvm/app.ts
+++ mvvm/app.ts
@@ -1,8 +1,20 @@
 import express from 'express'
+import { AccountModel } from './models/Entity/AccountModel'
+import { AccountViewModel } from './viewmodels/AccountViewModel'
+import { AccountView } from './views/AccountView'
 
 const PORT = 8001
 const app = express()
+app.use(express.json())
 
+const client = { name: 'Valdir' }
+const accountModel = new AccountModel('19001', client, 100, 2000)
+const accountViewModel = new AccountViewModel(accountModel)
+const accountView = new AccountView(accountViewModel)
+
+app.get("/conta", accountView.accountRender.bind(accountView));
+app.post("/transacao", accountView.setTransaction.bind(accountView));
+// app.post("/limiteCredito", accountView.setLimitCreditgit.bind(accountView));
 
 app.listen(PORT, () => {
     console.log(`Application running in port: ${PORT}`)
